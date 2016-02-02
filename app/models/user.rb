71e123a16f97249bfd0604a4faea5a2f12c9fba3
Marc Cruz <fcpmarcus@gmail.com>
2016-02-01 23:06:01 -0800

    user validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	
+	validates_presence_of :name
+	validates_presence_of :email
+	validates_presence_of :password	
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,19 @@ RSpec.describe User, type: :model do
   end
 
   describe "validations" do
-  	it "should not let user be created without email address" do
+  	it "should not let user be created without a name" do
+  	  @user.name = nil
+  	  expect(@user).to_not be_valid
+  	end
+
+  	it "should not let user be created without an email address" do
   	  @user.email = nil
   	  expect(@user).to_not be_valid
   	end
+  	it "should not let user be created without a password" do
+  	  @user.password = nil
+  	  expect(@user).to_not be_valid
+  	end
+
   end
 end
