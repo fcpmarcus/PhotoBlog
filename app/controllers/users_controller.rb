7350beb34ef6e330c03a782d0db9dfd729f02ca1
Marc Cruz <fcpmarcus@gmail.com>
2016-02-09 22:43:02 -0800

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,12 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+  def create
+  	@user = User.find(params[:user])
+  	if @user.save
+  	else
+  		render 'new'
+  	end
+  end
+
 end
