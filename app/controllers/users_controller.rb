2332f9ae0e51ff03a8a1bb6b7ca1a2390bbc2bbb
Marc Cruz <fcpmarcus@gmail.com>
2016-02-11 23:45:41 -0800

    flash message, test new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		flash[:success] = "Welcome to the Photo Blog"
+  		redirect_to @user
   	else
   		render 'new'
   	end
