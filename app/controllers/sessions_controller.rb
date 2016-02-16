31fdae40c4f30b8864d71eb476c38d0cf90d6692
Marc Cruz <fcpmarcus@gmail.com>
2016-02-15 22:22:46 -0800

    login sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
+  		log_in user
+  		redirect_to user
   		# Log in the user and redirect to the user show page
   	else
-  		flash[:danger] = 'Wrong email/password combination'
+  		flash.now[:danger] = 'Wrong email/password combination'
   		render 'new'
   	end
   end
