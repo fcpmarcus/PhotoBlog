e024693a7662bfbaff3954b4d0d4536fcfbabf0b
Marc Cruz <fcpmarcus@gmail.com>
2016-02-15 21:29:39 -0800

    session routes, login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
-  root                   'main#home'
-  get 'main/about'    => 'main#about'
-  get 'main/contact'  => 'main#contact'
-  get 'signup'        => 'users#new'
+  
+  root                       'main#home'
+  get     'main/about'    => 'main#about'
+  get     'main/contact'  => 'main#contact'
+  get     'signup'        => 'users#new'
+  get     'login'         => 'sessions#new'
+  post    'login'         => 'sessions#create'
+  delete  'logout'        => 'sessions#destroy' 
 
   resources :posts
   resources :users
