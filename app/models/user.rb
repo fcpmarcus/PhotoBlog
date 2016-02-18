d977f54f9cb6b9e02febe47f20da80a513fb3da4
Marc Cruz <fcpmarcus@gmail.com>
2016-02-17 22:43:10 -0800

    user remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
+
 	before_save { self.email = email.downcase }
 	
 	validates :name, presence: true, length: { maximum: 50 }
@@ -10,4 +12,22 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates :password_confirmation, presence: true
+
+	# Returns the hash digest of the given string
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                 	  BCrypt::Engine.cost
+    	BCrypt::Password.create(string, cost: cost)
+	end
+
+	# Returns a random token
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	# Remember a user in the db for use in persistent session
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remeber_digest, User.digest(remember_token))
+	end
 end
