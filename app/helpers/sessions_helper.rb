0330b1d5757b70e32b1f12aa198bc488f1054564
Marc Cruz <fcpmarcus@gmail.com>
2016-02-23 17:53:51 -0800

    forgetting user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,8 +29,15 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	# Forgets a persistant session
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
 	# Log out the current user
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
