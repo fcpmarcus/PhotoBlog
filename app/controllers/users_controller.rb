63dacf3ea19ae0d3c5022c42201feff023a08027
Marc Cruz <fcpmarcus@gmail.com>
2016-02-29 19:06:37 -0800

    users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
-  def new
-  	@user = User.new
+  def index
+    @users = User.all
   end
+
   def show
   	@user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
   def create
   	@user = User.new(user_params)
   	if @user.save
