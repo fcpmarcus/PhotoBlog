699fba4cc47ab1ed7fc586ab13e33fa3268702ac
Marc Cruz <fcpmarcus@gmail.com>
2016-03-23 00:38:44 -0700

    update session ctrlr for account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		log_in user
-  		params[:session][:remember_me] == '1' ?  remember(user) : forget(user)
-  		redirect_back_or user
-      
+      if user.activated?  
+    		log_in user
+    		params[:session][:remember_me] == '1' ?  remember(user) : forget(user)
+    		redirect_back_or user
+      else
+        message = "Account not activated."
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
   		# Log in the user and redirect to the user show page
   	else
   		flash.now[:danger] = 'Wrong email/password combination'
