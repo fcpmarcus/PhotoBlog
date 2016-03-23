6d88ccd6c750a871caa150c45727d8a616cf5b4a
Marc Cruz <fcpmarcus@gmail.com>
2016-03-22 19:08:00 -0700

    account activation ctrlr

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,8 @@ class User < ActiveRecord::Base
 	end
 
 	# Returns true if the given token matches the digest
-	def authenticated?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
 		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
